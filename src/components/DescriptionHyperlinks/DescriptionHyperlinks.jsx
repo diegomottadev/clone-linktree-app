be2c3f998d9db09e0f2e23cb7f7bc78e860d4606
Diego Motta <diego.motta@duradigital.com>
2026-05-13 16:51:47 -0300

    fix: rename DescriptionHyperlinks classes to avoid collision with Bio

--- src/components/DescriptionHyperlinks/DescriptionHyperlinks.jsx
+++ src/components/DescriptionHyperlinks/DescriptionHyperlinks.jsx
@@ -4,8 +4,8 @@ import "./DescriptionHyperlinks.css";
 const DescriptionHyperlinks = ({onDescription}) => {
    const {content_1} = onDescription
   return (
-    <div className="bio-box">
-      <p className="bio" style={{"textAlign": "center"}}>
+    <div className="description-box">
+      <p className="description-text">
         {/* Hi there! 👋I'm a self-taught and passionate software developer based in Argentina. With over 9 years of experience as a fullstack developer web, I bring a wealth of expertise to every project I undertake. */}
         <b>{content_1}</b>
       </p>
